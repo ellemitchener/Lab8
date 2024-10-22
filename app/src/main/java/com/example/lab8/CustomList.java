7444e8025266f3a5403ba396db7fc98b24fc750d
ellemitchener <mitchene@ualberta.ca>
2024-10-22 17:26:05 -0600

    added test custumlist java and add city method

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -48,8 +48,21 @@ public class CustomList extends ArrayAdapter<City> {
     public int getCount(){
         return cities.size();
     }
-
     public void addCity(City city){
+        /**
+         * this gets size of the list
+         * @return
+         */
+        public int getCount(){
+            return cities.size();
+        }
+        /**
+         * this adds a city object to the list
+         *for the first phase it will be
+         empty * @param city
+         */
+        public void addCity(City city){
+        }
 
     }
 
